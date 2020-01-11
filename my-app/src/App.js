f3de49912eaf83c182c4cc067d0d64aafc7535ec
RuslanPr0g <59767834+RuslanPr0g@users.noreply.github.com>
2020-01-12 01:20:16 +0300

    nre\

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,9 +4,45 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+     <div className='block'>
+      <div className="business-card">
+        <div className="front">
+          <div className="container-sm">
+            <img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Inkscape.logo.svg/390px-Inkscape.logo.svg.png"></img>
+          </div>
+            <div className="container">
+              <ul className="social">
+                <li><a href="#">Design</a></li>
+                <li><a href="#">aaaa@aaaa.com</a></li>
+                <li><a href="#">666 666 666</a></li>
+                <li><a href="#">CodePen</a></li>
+                <li><a href="#">GitHub</a></li>
+                <li><a href="#">Reddit</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="business-card">
+        <div className="front">
+          <div className="container-sm">
+            <img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Inkscape.logo.svg/390px-Inkscape.logo.svg.png"></img>
+          </div>
+            <div className="container">
+              <ul className="social">
+                <li><a href="#">Design</a></li>
+                <li><a href="#">aaaa@aaaa.com</a></li>
+                <li><a href="#">666 666 666</a></li>
+                <li><a href="#">CodePen</a></li>
+                <li><a href="#">GitHub</a></li>
+                <li><a href="#">Reddit</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
     </div>
+    
+
   );
 }
 
