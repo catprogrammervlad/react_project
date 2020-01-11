513c8fe605caa49d90e35c30e5d5318392b0103b
catprogrammer <catprogrammer.work@gmail.com>
2020-01-11 22:35:52 +0200

    m app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,7 @@ import Person from './Person/Person'
 
 class App extends Component {
   state = {
-    Persons: [
+    persons: [
       {
         code: 'ff44g43',
         name: 'rick',
@@ -13,85 +13,52 @@ class App extends Component {
         rank: 'sergeant'
       },
       {
-        code: 'ff44g43',
-        name: 'rick',
-        surname: 'vilson',
-        residence: 'new-york',
-        rank: 'sergeant'
-      },
-      {
-        code: 'ff44g43',
-        name: 'rick',
-        surname: 'vilson',
-        residence: 'new-york',
-        rank: 'sergeant'
-      },
-      {
-        code: 'ff44g43',
-        name: 'rick',
-        surname: 'vilson',
-        residence: 'new-york',
-        rank: 'sergeant'
-      },
-      {
-        code: 'ff44g43',
-        name: 'rick',
-        surname: 'vilson',
-        residence: 'new-york',
-        rank: 'sergeant'
-      },
-      {
-        code: 'ff44g43',
-        name: 'rick',
-        surname: 'vilson',
-        residence: 'new-york',
-        rank: 'sergeant'
-      },
-      {
-        code: 'ff44g43',
-        name: 'rick',
-        surname: 'vilson',
-        residence: 'new-york',
-        rank: 'sergeant'
-      },
-      {
-        code: 'ff44g43',
-        name: 'rick',
-        surname: 'vilson',
-        residence: 'new-york',
-        rank: 'sergeant'
+        code: 'ag55j32',
+        name: 'Gaybos',
+        surname: 'Stashon',
+        residence: 'Florida',
+        rank: 'marshal'
       }
     ]
+  }
+
+  ChangeTheme = () => {
+    this.setState({
+      persons: [
+        {
+          code: 'ff4',
+          name: this.state.persons[0].name,
+          surname: this.state.persons[0].surname,
+          residence: this.state.persons[0].residence,
+          rank: this.state.persons[0].rank
+        },
+        {
+          code: 'ag5',
+          name: this.state.persons[1].name,
+          surname: this.state.persons[1].surname,
+          residence: this.state.persons[1].residence,
+          rank: this.state.persons[1].rank
+        }
+      ]
+    })
   }
   render() {
     return (
       <div>
+        <button onClick={this.ChangeTheme}>Short code</button>
         <Person
-          code={this.state.Persons[0].code}
-          name={this.state.Persons[0].name}
-          surname={this.state.Persons[0].surname}
-          residence={this.state.Persons[0].residence}
-          rank={this.state.Persons[0].rank}></Person>
-        <Person
-          code={this.state.Persons[0].code}
-          name={this.state.Persons[0].name}
-          surname={this.state.Persons[0].surname}
-          residence={this.state.Persons[0].residence}
-          rank={this.state.Persons[0].rank}></Person>
+          code={this.state.persons[0].code}
+          name={this.state.persons[0].name}
+          surname={this.state.persons[0].surname}
+          residence={this.state.persons[0].residence}
+          rank={this.state.persons[0].rank}></Person>
 
         <Person
-          code={this.state.Persons[0].code}
-          name={this.state.Persons[0].name}
-          surname={this.state.Persons[0].surname}
-          residence={this.state.Persons[0].residence}
-          rank={this.state.Persons[0].rank}></Person>
-
-        <Person
-          code={this.state.Persons[0].code}
-          name={this.state.Persons[0].name}
-          surname={this.state.Persons[0].surname}
-          residence={this.state.Persons[0].residence}
-          rank={this.state.Persons[0].rank}></Person>
+          code={this.state.persons[1].code}
+          name={this.state.persons[1].name}
+          surname={this.state.persons[1].surname}
+          residence={this.state.persons[1].residence}
+          rank={this.state.persons[1].rank}></Person>
       </div>
     )
   }
